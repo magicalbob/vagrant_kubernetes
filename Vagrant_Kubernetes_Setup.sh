4a60f76e07396955c0e9c1b9f78b6d7e05afc7e9
= <=>
2023-08-06 11:47:52 +0100

    clone github project that will actually set up the kubernetes cluster

--- Vagrant_Kubernetes_Setup.sh
+++ Vagrant_Kubernetes_Setup.sh
@@ -5,3 +5,6 @@ vagrant up
 
 # Set up ssh from node 1 to nodes 1 through 5
 ./setup_ssh.sh
+
+# Clone the project to do the actual kubernetes cluster setup
+vagrant ssh -c 'git clone https://github.com/kubernetes-sigs/kubespray.git' node1
