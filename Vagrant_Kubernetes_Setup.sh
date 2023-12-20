6819c600df3f5ea8c756bc98f35f525bc0c5c884
Ian Ellis <ellisiana@gail.com>
2023-12-20 20:50:29 +0000

    print statements instead of boring old comment lines

--- Vagrant_Kubernetes_Setup.sh
+++ Vagrant_Kubernetes_Setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Work out primary network adapter for Mac or linux
+echo Work out primary network adapter for Mac or linux
 if [[ $(uname) == "Darwin" ]]; then
   # For macOS
   PRIMARY_ADAPTER=$(route get default | grep interface | awk '{print $2}')
@@ -11,7 +11,7 @@ fi
 echo "Primary Adapter: ${PRIMARY_ADAPTER}"
 export PRIMARY_ADAPTER
 
-# Read configuration from config.json
+echo Read configuration from config.json
 CONTROL_NODES=$(jq -r '.control_nodes' config.json)
 WORKER_NODES=$(jq -r '.worker_nodes' config.json)
 export TOTAL_NODES=$((CONTROL_NODES + WORKER_NODES))
@@ -20,22 +20,22 @@ export CPU_COUNT=$(jq -r '.cpu_count' config.json)
 export PUB_NET=$(jq -r '.pub_net' config.json)
 export KUBE_VERSION=$(jq -r '.kube_version' config.json)
 
-# Create Vagrantfile from template
+echo Create Vagrantfile from template
 envsubst < Vagrantfile.template > Vagrantfile
 
-# Bring up all the nodes without provisioning
+echo Bring up all the nodes without provisioning
 vagrant up --no-provision
 
-# Loop to check if all nodes are created and then provision
+echo Loop to check if all nodes are created and then provision
 while vagrant status | grep -q "not created (virtualbox)"; do
   echo "Not all nodes are created yet. Retrying..."
   vagrant up --no-provision
 done
 
-# Provision all the nodes
+echo Provision all the nodes
 vagrant provision
 
-# Generate the hosts.yaml content
+echo Generate the hosts.yaml content
 HOSTS_YAML="all:
   hosts:"
 
@@ -86,53 +86,53 @@ HOSTS_YAML+="
     calico_rr:
       hosts: {}"
 
-# Write the hosts.yaml content to the file
+echo Write the hosts.yaml content to the file
 echo "$HOSTS_YAML" > hosts.yaml
 
-# Set up ssh between the nodes
-# First copy the insecure_private_key to node1 (which is the only node that needs it)
+echo Set up ssh between the nodes
+echo First copy the insecure_private_key to node1 (which is the only node that needs it)
 vagrant upload ~/.vagrant.d/insecure_private_key /home/vagrant/.ssh/id_rsa node1
 
-# Now create the public key from it
+echo Now create the public key from it
 ssh-keygen -y -f ~/.vagrant.d/insecure_private_key > ./insecure_public_key
 
-# Copy the public key to each node
+echo Copy the public key to each node
 for i in $(seq 1 $TOTAL_NODES); do
   vagrant upload ./insecure_public_key /home/vagrant/.ssh/id_rsa.pub node$i
 done
 
-# Append the public key to the authorized_keys file on each node
+echo Append the public key to the authorized_keys file on each node
 for i in $(seq 1 $TOTAL_NODES); do
   vagrant ssh -c 'cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys' node$i
 done
 
-# Do an intial ssh to each node (including node1) from node1 
+echo Do an intial ssh to each node (including node1) from node1 
 for i in $(seq 1 $TOTAL_NODES); do
   vagrant ssh -c "echo uptime|ssh -o StrictHostKeyChecking=no ${PUB_NET}.20${i}" node1
 done
 
-# Clone the project to do the actual kubernetes cluster setup
+echo Clone the project to do the actual kubernetes cluster setup
 vagrant ssh -c 'rm -rf /home/vagrant/kubespray' node1
 vagrant ssh -c 'git clone https://github.com/kubernetes-sigs/kubespray.git /home/vagrant/kubespray' node1
 
 
-# Python requirements
+echo Python requirements
 vagrant ssh -c 'sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3.10-venv' node1
 vagrant ssh -c 'python3 -m venv /home/vagrant/.py3kubespray'  node1
 vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && pip install -r /home/vagrant/kubespray/requirements.txt'  node1
 
-# Write /etc/hosts file
-# Do an intial ssh to each node (including node1) from node1
+echo Write /etc/hosts file
+echo Do an intial ssh to each node (including node1) from node1
 cp hosts.template hosts
 for i in $(seq 1 $TOTAL_NODES); do
   echo 192.168.56.20${i} node${i} >> hosts
 done
-# Copy hosts file to each node
+echo Copy hosts file to each node
 for i in $(seq 1 $TOTAL_NODES); do
   vagrant ssh -c "sudo cp /vagrant/hosts /etc/hosts" node1
 done
 
-# Set up the cluster
+echo Set up the cluster
 vagrant ssh -c 'cp -rfp /home/vagrant/kubespray/inventory/sample /home/vagrant/kubespray/inventory/vagrant_kubernetes' node1
 vagrant ssh -c "sed -i -E '/^kube_version:/s/.*/kube_version: $KUBE_VERSION/'  /home/vagrant/kubespray/inventory/vagrant_kubernetes/group_vars/k8s_cluster/k8s-cluster.yml" node1
 vagrant ssh -c 'declare -a IPS=(); for ((i=1; i<=TOTAL_NODES; i++)); do IPS+=("${PUB_NET}.20$i"); done && . /home/vagrant/.py3kubespray/bin/activate && CONFIG_FILE=/home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml python3 /home/vagrant/kubespray/contrib/inventory_builder/inventory.py "${IPS[@]}"' node1
@@ -140,26 +140,26 @@ vagrant ssh -c 'cp /vagrant/hosts.yaml /home/vagrant/kubespray/inventory/vagrant
 vagrant ssh -c 'export PUB_NET=192.168.56; export TOTAL_NODES=7; declare -a IPS=(); for ((i=1; i<=TOTAL_NODES; i++)); do IPS+=("${PUB_NET}.20$i"); done; echo "${IPS[@]}" && . /home/vagrant/.py3kubespray/bin/activate && CONFIG_FILE=/home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml python3 /home/vagrant/kubespray/contrib/inventory_builder/inventory.py ${IPS[@]}' node1
 vagrant ssh -c 'cp /vagrant/addons.yml /home/vagrant/kubespray/inventory/vagrant_kubernetes/group_vars/k8s_cluster/addons.yml' node1
 
-# Uncomment upstream dns servers in all.yaml
+echo Uncomment upstream dns servers in all.yaml
 vagrant ssh -c 'sed -i "/upstream_dns_servers:/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' node1
 vagrant ssh -c 'sed -i "/- 8.8.8.8/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' node1
 vagrant ssh -c 'sed -i "/- 8.8.4.4/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' node1
 
-# Disable firewalls, enable IPv4 forwarding and switch off swap
+echo Disable firewalls, enable IPv4 forwarding and switch off swap
 vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "sudo systemctl stop firewalld && sudo systemctl disable firewalld"' node1
 vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "echo net.ipv4.ip_forward=1 | sudo tee -a /etc/sysctl.conf"' node1
 vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "sudo sed -i \"/ swap / s/^\(.*\)$/#\1/g\" /etc/fstab && sudo swapoff -a"' node1
 
-# Do install of kubernetes
+echo Do install of kubernetes
 vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && cd /home/vagrant/kubespray && ansible-playbook -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml --become --become-user=root /home/vagrant/kubespray/cluster.yml' node1
 
-# Now copy /root/.kube/config to vagrant user
+echo Now copy /root/.kube/config to vagrant user
 vagrant ssh -c 'mkdir -p /home/vagrant/.kube' node1
 vagrant ssh -c 'sudo cp /root/.kube/config /home/vagrant/.kube/config' node1
 vagrant ssh -c 'sudo chown vagrant:vagrant /home/vagrant/.kube/config' node1
 
-# Install helm
+echo Install helm
 vagrant ssh -c 'sudo snap install helm --classic' node1
 
-# Echo message to show script completion
+echo Echo message to show script completion
 echo "Script `basename $0` has finished"
