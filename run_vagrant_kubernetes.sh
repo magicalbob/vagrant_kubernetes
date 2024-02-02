0abedcfb199edaf856532a0dd48215daf07506ee
Ian Ellis <ellisiana@gmail.com>
2024-02-02 21:41:26 +0000

    make sure all messages go in the log

--- run_vagrant_kubernetes.sh
+++ run_vagrant_kubernetes.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]
-then
+if [ $# -ne 1 ]; then
   echo "Have to supply a verb"
   exit 1
 fi
 
-./Vagrant_Kubernetes_Setup.sh $1 2>&1 > ./vagrant_kubernetes.log &
+./Vagrant_Kubernetes_Setup.sh $1 > ./vagrant_kubernetes.log 2>&1 &
+
