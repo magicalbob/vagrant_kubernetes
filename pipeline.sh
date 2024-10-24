f3737ff089bc490518fe662c2ebbcd2d164bfdc6
Ian Ellis <ellisiana@gmail.com>
2024-10-24 08:12:33 +0100

    better vagrant mock

--- pipeline.sh
+++ pipeline.sh
@@ -7,11 +7,10 @@ sudo apt-get install -f
 sudo apt-get install -y python3-dev python3-pip iproute2 jq gettext virtualbox yq ruby-full
 sudo wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo dpkg -i vagrant_2.2.19_x86_64.deb
-mv vagrant /usr/local/bin/
-which vagrant
-#./Vagrant_Kubernetes_Setup.sh UP_ONLY
-#cat Vagrantfile
-#echo "Checking Ruby syntax..."
-#ruby -c Vagrantfile || { echo "Ruby syntax error."; exit 1; }
-#./Vagrant_Kubernetes_Setup.sh SKIP_UP
-#cat hosts.yaml | yq eval
+mv vagrant /usr/local/bin/ 2>/dev/null
+./Vagrant_Kubernetes_Setup.sh UP_ONLY
+cat Vagrantfile
+echo "Checking Ruby syntax..."
+ruby -c Vagrantfile || { echo "Ruby syntax error."; exit 1; }
+./Vagrant_Kubernetes_Setup.sh SKIP_UP
+cat hosts.yaml | yq eval
