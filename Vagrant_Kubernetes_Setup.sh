56a790e6ed010f7fa33e2902be423a152b38293b
Ian Ellis <ellisiana@gmail.com>
2024-12-15 16:52:39 +0000

    added node_name to config.json

--- Vagrant_Kubernetes_Setup.sh
+++ Vagrant_Kubernetes_Setup.sh
@@ -33,6 +33,7 @@ export CPU_COUNT=$(jq -r '.cpu_count' config.json)
 export PUB_NET=$(jq -r '.pub_net' config.json)
 export KUBE_VERSION=$(jq -r '.kube_version' config.json)
 export KUBESPRAY_VERSION=$(jq -r '.kubespray_version' config.json)
+export NODE_NAME=$(jq -r '.node_name' config.json)
 
 echo Create Vagrantfile from template
 envsubst < Vagrantfile.template > Vagrantfile
@@ -54,41 +55,41 @@ else
 
     echo "Update and upgrade each node" 
     for i in $(seq 1 $TOTAL_NODES); do
-      vagrant ssh -c 'sudo apt-get update' node$i
-      vagrant ssh -c 'sudo apt-get upgrade -y' node$i
-      vagrant ssh -c 'sudo apt-get install -y net-tools' node$i
+      vagrant ssh -c 'sudo apt-get update' $NODE_NAME$i
+      vagrant ssh -c 'sudo apt-get upgrade -y' $NODE_NAME$i
+      vagrant ssh -c 'sudo apt-get install -y net-tools' $NODE_NAME$i
     done
 
     echo "Write /etc/hosts file"
-    echo "Do an intial ssh to each node from node1"
+    echo "Do an intial ssh to each node from $NODE_NAME1"
     cp hosts.template hosts
     for i in $(seq 1 $TOTAL_NODES); do
-      echo ${PUB_NET}.22${i} node${i} >> hosts
+      echo ${PUB_NET}.22${i} $NODE_NAME${i} >> hosts
     done
     echo Copy hosts file to each node
     for i in $(seq 1 $TOTAL_NODES); do
-      vagrant ssh -c "sudo cp /vagrant/hosts /etc/hosts" node${i}
+      vagrant ssh -c "sudo cp /vagrant/hosts /etc/hosts" $NODE_NAME${i}
     done
 
     echo Set up ssh between the nodes
-    vagrant upload ~/.vagrant.d/insecure_private_key /home/vagrant/.ssh/id_rsa node1
+    vagrant upload ~/.vagrant.d/insecure_private_key /home/vagrant/.ssh/id_rsa $NODE_NAME1
 
     echo Now create the public key from it
     ssh-keygen -y -f ~/.vagrant.d/insecure_private_key > ./insecure_public_key
 
     echo Copy the public key to each node
     for i in $(seq 1 $TOTAL_NODES); do
-      vagrant upload ./insecure_public_key /home/vagrant/.ssh/id_rsa.pub node$i
+      vagrant upload ./insecure_public_key /home/vagrant/.ssh/id_rsa.pub $NODE_NAME$i
     done
 
     echo Append the public key to the authorized_keys file on each node
     for i in $(seq 1 $TOTAL_NODES); do
-      vagrant ssh -c 'cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys' node$i
+      vagrant ssh -c 'cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys' $NODE_NAME$i
     done
 
-    echo Do an intial ssh to each node from node1 
+    echo "Do an intial ssh to each node from $NODE_NAME1" 
     for i in $(seq 1 $TOTAL_NODES); do
-      vagrant ssh -c "echo uptime|ssh -o StrictHostKeyChecking=no ${PUB_NET}.22${i}" node1
+      vagrant ssh -c "echo uptime|ssh -o StrictHostKeyChecking=no ${PUB_NET}.22${i}" "$NODE_NAME1"
     done
 
     echo "Script `basename $0` has finished"
@@ -102,7 +103,7 @@ HOSTS_YAML="all:
 
 for i in $(seq 1 $TOTAL_NODES); do
   HOSTS_YAML+="
-    node$i:
+    $NODE_NAME$i:
       ansible_host: ${PUB_NET}.22$i
       ip: ${PUB_NET}.22$i
       access_ip: ${PUB_NET}.22$i"
@@ -115,7 +116,7 @@ HOSTS_YAML+="
 
 for i in $(seq 1 $CONTROL_NODES); do
   HOSTS_YAML+="
-        node$i:"
+        $NODE_NAME$i:"
 done
 
 HOSTS_YAML+="
@@ -124,7 +125,7 @@ HOSTS_YAML+="
 
 for i in $(seq $((CONTROL_NODES + 1)) $((TOTAL_NODES))); do
   HOSTS_YAML+="
-        node$i:"
+        $NODE_NAME$i:"
 done
 
 HOSTS_YAML+="
@@ -132,7 +133,7 @@ HOSTS_YAML+="
       hosts:"
 for i in $(seq 1 $CONTROL_NODES); do
   HOSTS_YAML+="
-        node$i:"
+        $NODE_NAME$i:"
 done
 
 HOSTS_YAML+="
@@ -151,7 +152,7 @@ echo "Write the hosts.yaml content to the file"
 echo "$HOSTS_YAML" > hosts.yaml
 
 echo "Clone the project to do the actual kubernetes cluster setup"
-vagrant ssh node1 -c '
+vagrant ssh "$NODE_NAME1" -c '
     MAX_ATTEMPTS=3
     ATTEMPT=1
     while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
@@ -173,55 +174,55 @@ vagrant ssh node1 -c '
 
 if [ ! -z "$KUBESPRAY_VERSION" ] && [ "$KUBESPRAY_VERSION" != "null" ]; then
     echo "Checkout tag $KUBESPRAY_VERSION"
-    vagrant ssh node1 -c "cd /home/vagrant/kubespray && git checkout $KUBESPRAY_VERSION"
+    vagrant ssh "$NODE_NAME1" -c "cd /home/vagrant/kubespray && git checkout $KUBESPRAY_VERSION"
 fi
 
 echo "Python requirements"
-vagrant ssh -c 'sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3.10-venv' node1
-vagrant ssh -c 'python3 -m venv /home/vagrant/.py3kubespray'  node1
-vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && pip install -r /home/vagrant/kubespray/requirements.txt'  node1
+vagrant ssh -c 'sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3.10-venv' "$NODE_NAME1"
+vagrant ssh -c 'python3 -m venv /home/vagrant/.py3kubespray'  "$NODE_NAME1"
+vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && pip install -r /home/vagrant/kubespray/requirements.txt'  "$NODE_NAME1"
 
 echo Set up the cluster
-vagrant ssh -c 'cp -rfp /home/vagrant/kubespray/inventory/sample /home/vagrant/kubespray/inventory/vagrant_kubernetes' node1
-vagrant ssh -c "sed -i -E \"/^kube_version:/s/.*/kube_version: $KUBE_VERSION/\"  /home/vagrant/kubespray/inventory/vagrant_kubernetes/group_vars/k8s_cluster/k8s-cluster.yml" node1
-vagrant ssh -c 'cp /vagrant/hosts.yaml /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml' node1
+vagrant ssh -c 'cp -rfp /home/vagrant/kubespray/inventory/sample /home/vagrant/kubespray/inventory/vagrant_kubernetes' "$NODE_NAME1"
+vagrant ssh -c "sed -i -E \"/^kube_version:/s/.*/kube_version: $KUBE_VERSION/\"  /home/vagrant/kubespray/inventory/vagrant_kubernetes/group_vars/k8s_cluster/k8s-cluster.yml" "$NODE_NAME1"
+vagrant ssh -c 'cp /vagrant/hosts.yaml /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml' "$NODE_NAME1"
 envsubst < build_inventory.template | sed 's/PUBLIC_NET.i/$PUBLIC_NET.$i/' > build_inventory.sh
 chmod +x build_inventory.sh
 echo Execute build_inventory.sh
-vagrant ssh -c 'bash -c /vagrant/build_inventory.sh' node1
+vagrant ssh -c 'bash -c /vagrant/build_inventory.sh' "$NODE_NAME1"
 
-vagrant ssh -c 'cp /vagrant/addons.yml /home/vagrant/kubespray/inventory/vagrant_kubernetes/group_vars/k8s_cluster/addons.yml' node1
+vagrant ssh -c 'cp /vagrant/addons.yml /home/vagrant/kubespray/inventory/vagrant_kubernetes/group_vars/k8s_cluster/addons.yml' "$NODE_NAME1"
 
 echo Uncomment upstream dns servers in all.yaml
-vagrant ssh -c 'sed -i "/upstream_dns_servers:/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' node1
-vagrant ssh -c 'sed -i "/- 8.8.8.8/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' node1
-vagrant ssh -c 'sed -i "/- 8.8.4.4/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' node1
+vagrant ssh -c 'sed -i "/upstream_dns_servers:/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' "$NODE_NAME1"
+vagrant ssh -c 'sed -i "/- 8.8.8.8/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' "$NODE_NAME1"
+vagrant ssh -c 'sed -i "/- 8.8.4.4/s/^# *//" ~/kubespray/inventory/vagrant_kubernetes/group_vars/all/all.yml' "$NODE_NAME1"
 
 echo Disable firewalls, enable IPv4 forwarding and switch off swap
-vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "sudo systemctl stop firewalld && sudo systemctl disable firewalld"' node1
-vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "echo net.ipv4.ip_forward=1 | sudo tee -a /etc/sysctl.conf"' node1
-vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "sudo sed -i \"/ swap / s/^\(.*\)$/#\1/g\" /etc/fstab && sudo swapoff -a"' node1
+vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "sudo systemctl stop firewalld && sudo systemctl disable firewalld"' "$NODE_NAME1"
+vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "echo net.ipv4.ip_forward=1 | sudo tee -a /etc/sysctl.conf"' "$NODE_NAME1"
+vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && ansible all -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml -m shell -a "sudo sed -i \"/ swap / s/^\(.*\)$/#\1/g\" /etc/fstab && sudo swapoff -a"' "$NODE_NAME1"
 
 echo Do install of kubernetes
-vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && cd /home/vagrant/kubespray && ansible-playbook -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml --become --become-user=root /home/vagrant/kubespray/cluster.yml' node1
+vagrant ssh -c '. /home/vagrant/.py3kubespray/bin/activate && cd /home/vagrant/kubespray && ansible-playbook -i /home/vagrant/kubespray/inventory/vagrant_kubernetes/hosts.yaml --become --become-user=root /home/vagrant/kubespray/cluster.yml' '$NODE_NAME1"
 
 echo Now copy /root/.kube/config to vagrant user
-vagrant ssh -c 'mkdir -p /home/vagrant/.kube' node1
-vagrant ssh -c 'sudo cp /root/.kube/config /home/vagrant/.kube/config' node1
-vagrant ssh -c 'sudo chown vagrant:vagrant /home/vagrant/.kube/config' node1
+vagrant ssh -c 'mkdir -p /home/vagrant/.kube' "$NODE_NAME1"
+vagrant ssh -c 'sudo cp /root/.kube/config /home/vagrant/.kube/config' "$NODE_NAME1"
+vagrant ssh -c 'sudo chown vagrant:vagrant /home/vagrant/.kube/config' "$NODE_NAME1"
 
 echo Install helm
-vagrant ssh -c 'sudo snap install helm --classic' node1
+vagrant ssh -c 'sudo snap install helm --classic' "$NODE_NAME1"
 
 echo Install Metrics Server
-vagrant ssh -c 'kubectl apply -f https://dev.ellisbs.co.uk/files/components.yaml' node1
+vagrant ssh -c 'kubectl apply -f https://dev.ellisbs.co.uk/files/components.yaml' '$NODE_NAME1"
 
 if [ ! -z "$OPENAI_API_KEY" ]
 then
   echo Install k8sgpt
-  vagrant ssh -c "curl -Lo /tmp/k8sgpt.deb https://github.com/k8sgpt-ai/k8sgpt/releases/download/v0.3.24/k8sgpt_$(uname -m|sed 's/x86_64/amd64/').deb" node1
-  vagrant ssh -c 'sudo dpkg -i /tmp/k8sgpt.deb' node1
-  vagrant ssh -c "k8sgpt auth add --backend openai --model gpt-3.5-turbo --password $OPENAI_API_KEY" node1
+  vagrant ssh -c "curl -Lo /tmp/k8sgpt.deb https://github.com/k8sgpt-ai/k8sgpt/releases/download/v0.3.24/k8sgpt_$(uname -m|sed 's/x86_64/amd64/').deb" "$NODE_NAME1"
+  vagrant ssh -c 'sudo dpkg -i /tmp/k8sgpt.deb' "$NODE_NAME1"
+  vagrant ssh -c "k8sgpt auth add --backend openai --model gpt-3.5-turbo --password $OPENAI_API_KEY" "$NODE_NAME1"
 fi
 
 echo "Script `basename $0` has finished"
